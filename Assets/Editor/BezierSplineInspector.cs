4e1f6a37d3e84a61b2dde477ddfd4d5935e5d50b
ThatSoloDev <elias.wt@hotmail.com>
2020-07-08 15:22:44 +0200

    Initial commit

---
+++ Assets/Editor/BezierSplineInspector.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+//ToDo: Refactor
+
+[CustomEditor(typeof(BezierSpline))]
+[CanEditMultipleObjects]
+public class BezierSplineInspector : Editor //Refactor
+{
+    private int selectedIndex = -1;
+
+    private BezierSpline _bezierSpline;
+    private Transform _handleTransform;
+    private Quaternion _handleRotation;
+
+    private Vector3[] _points;
+
+    private SerializedProperty data;
+
+    private SerializedProperty _lineSteps;
+    private SerializedProperty _directionSizeScale;
+    private SerializedProperty _directionNoiseScale;
+    private SerializedProperty _handleSize;
+    private SerializedProperty _pickSize;
+    private SerializedProperty _showVelocity;
+    private SerializedProperty _showPrimitive;
+
+
+    private void OnEnable()
+    {
+        _bezierSpline = target as BezierSpline;
+
+        data = serializedObject.FindProperty("splineVisualData");
+
+        _lineSteps = data.FindPropertyRelative("lineSteps");
+        _directionSizeScale = data.FindPropertyRelative("directionSizeScale");
+        _directionNoiseScale = data.FindPropertyRelative("directionNoiseScale");
+        _handleSize = data.FindPropertyRelative("handleSize");
+        _pickSize = data.FindPropertyRelative("pickSize");
+        _showVelocity = data.FindPropertyRelative("showVelocity");
+        _showPrimitive = data.FindPropertyRelative("showPrimitive");
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        Undo.RecordObject(_bezierSpline, "Modify Inspector");
+        EditorUtility.SetDirty(_bezierSpline);
+
+        EditorGUILayout.PropertyField(data);
+
+        EditorGUILayout.LabelField("Curves", new GUIStyle() { alignment = TextAnchor.MiddleCenter });
+        for (int i = 0; i < _bezierSpline.points.Count; i++)
+        {
+            Debug.Log("test");
+            if (i == 0)
+            {
+                _bezierSpline.points[i] = EditorGUILayout.Vector3Field("Start Point ", _bezierSpline.points[i]);
+                continue;
+            }
+
+            if ((i - 1) % 3 == 0)
+            {
+                EditorGUILayout.LabelField("Curve " + (i / 4 + 1));
+            }
+
+            _bezierSpline.points[i] = EditorGUILayout.Vector3Field("Point " + (i), _bezierSpline.points[i]);
+
+        }
+
+        if (GUILayout.Button("Add Curve"))
+        {
+            _bezierSpline.AddCurve();
+        }
+
+        if(GUILayout.Button("Remove Curve"))
+        {
+            _bezierSpline.RemoveCurve();
+        }
+
+        SceneView.RepaintAll();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void OnSceneGUI()
+    {
+        Undo.RecordObject(_bezierSpline, "Modify Spline");
+        EditorUtility.SetDirty(_bezierSpline);
+
+        _handleTransform = _bezierSpline.transform;
+        _handleRotation = Tools.pivotRotation == PivotRotation.Local ? _handleTransform.rotation : Quaternion.identity;
+
+        _points = new Vector3[_bezierSpline.points.Count];
+        for(int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = ShowPoint(i);
+        }
+
+        DrawPrimitive();
+        DrawBezierCurve(_lineSteps.floatValue * _bezierSpline.CurveCount);
+        DrawVelocity(_lineSteps.floatValue * _bezierSpline.CurveCount * _directionNoiseScale.floatValue);
+    }
+
+    private void DrawVelocity(float steps)
+    {
+        if (_showVelocity.boolValue == false)
+            return;
+
+        if (_directionNoiseScale.floatValue == 0)
+            return;
+
+        for (int i = 0; i < steps; i++)
+        {
+            Vector3 lineEnd = _bezierSpline.GetPoint(i / steps);
+            Handles.color = Color.green;
+            Handles.DrawLine(lineEnd, lineEnd + _bezierSpline.GetDirection(i / steps) * _directionSizeScale.floatValue);
+        }
+    }
+
+    private void DrawBezierCurve(float steps)
+    {
+        Vector3 lineStart = _bezierSpline.GetPoint(0f);
+
+        for (int y = 0; y < steps; y++)
+        {
+            Vector3 lineEnd = _bezierSpline.GetPoint(y / steps);
+            Handles.color = Color.white;
+            Handles.DrawLine(lineStart, lineEnd);
+            lineStart = lineEnd;
+        }
+    }
+
+    private void DrawPrimitive()
+    {
+        if (_showPrimitive.boolValue == false)
+            return;
+
+        for (int i = 0; i < _points.Length - 1; i++)
+        {
+            Handles.color = Color.gray;
+            Handles.DrawLine(_points[i], _points[i + 1]);
+        }
+    }
+
+    private Vector3 ShowPoint(int index)
+    {
+        Vector3 point = _handleTransform.TransformPoint(_bezierSpline.GetControlPoint(index));
+
+        float size = HandleUtility.GetHandleSize(point);
+        Handles.color = Color.white;
+
+        if(Handles.Button(point, _handleRotation, size * _handleSize.floatValue, size * _pickSize.floatValue, Handles.DotHandleCap))
+        {
+            selectedIndex = index;
+        }
+        if(selectedIndex == index)
+        {
+            EditorGUI.BeginChangeCheck();
+            point = Handles.DoPositionHandle(point, _handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                _bezierSpline.SetControlPoint(index, _handleTransform.InverseTransformPoint(point));
+            }
+        }
+
+        return point;
+    }
+}
---
+++ Assets/Spline/BezierSpline.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Spline
+{
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) //Gets point on curve that is t steps away from start point
+    {
+        Vector3 point = (1 - t) * (1 - t) * (1 - t) * p0 +
+            3f * (1 - t) * (1 - t) * t * p1 +
+            3f * (1 - t) * t * t * p2 +
+            t * t * t * p3;
+
+        return point;
+    }
+
+    public static Vector3 GetDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) //Gets derivative of point that is t steps away from start point
+    {
+        Vector3 derivative = 3f * (1 - t) * (1 - t) * (p1 - p0) +
+        6f * (1 - t) * t * (p2 - p1) +
+        3f * t * t * (p3 - p2);
+
+        return derivative;
+    }
+}
+
+[Serializable]
+public class BezierSplineData
+{
+    public bool showVelocity = false;
+    public bool showPrimitive = false;
+    [Range(0, 100)]
+    public float lineSteps = 50;
+
+    [Range(0, 2)]
+    public float directionSizeScale = 1, directionNoiseScale = 1;
+
+    [Range(0, 0.3f)]
+    public float handleSize = 0.1f, pickSize = 0.06f;
+}
+public class BezierSpline : MonoBehaviour
+{
+    public BezierSplineData splineVisualData;
+
+    public int CurveCount => (points.Count) / 3;
+
+    public List<Vector3> points = new List<Vector3>();
+
+    public void Reset()
+    {
+        points.Add(new Vector3(1, 0, 0));
+        AddCurve();
+    }
+
+    public Vector3 GetControlPoint (int index)
+    {
+        return points[index];
+    }
+
+    public void SetControlPoint(int index, Vector3 point)
+    {
+        points[index] = point;
+    }
+
+    public void AddCurve()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 point = new Vector3(points.Count + 1, 0f, 0f);
+
+            points.Add(point);
+        }
+    }
+
+    public void RemoveCurve()
+    {
+        if (CurveCount <= 1)
+            return;
+
+        for (int i = 0; i < 3; i++)
+        {
+            points.Remove(points[points.Count - 1]);
+        }
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        int i;
+        if(t >= 1f)
+        {
+            t = 1f;
+            i = points.Count - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+
+        Vector3 point = transform.TransformPoint(Spline.GetPoint(points[i], points[i + 1], points[i + 2], points[i + 3], t));
+
+        return point;
+    }
+
+    public Vector3 GetVelocity(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Count - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+
+        Vector3 velocity = transform.TransformPoint(Spline.GetDerivative(points[i], points[i + 1], points[i + 2], points[i + 3], t)) - transform.position;
+
+        return velocity;
+    }
+
+    public Vector3 GetDirection(float t)
+    {
+        Vector3 direction = GetVelocity(t).normalized;
+
+        return direction;
+    }
+}
